a658dfc520585b39e545dbc4588f8da31d9ab853
Ajay <goswamiajay526@gmail.com>
2025-10-03 11:29:11 +0530

    feat: Add @vercel/analytics package and integrate Analytics component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { ThemeProvider as CustomThemeProvider } from './contexts/ThemeContext.tsx';
 import ThemedApp from './components/ThemedApp.tsx';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CustomThemeProvider>
       <ThemedApp />
+      <Analytics />
     </CustomThemeProvider>
   </StrictMode>,
 )
